677e52ce510b3350896e87bd4426cb7b8328b336
Umut <bozdag321@hotmail.com>
2020-04-21 16:51:18 +0300

    add year and type support

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,28 +1,90 @@
 import React from "react";
-import { Form, Row, Col, Button } from "react-bootstrap";
+import { Form, Row, Col, Button, Container } from "react-bootstrap";
+import movieStore from "../../store/movieStore";
+import { observer } from "mobx-react";
+import "./search.module.scss";
+@observer
+export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      year: "",
+      input: "",
+      type: "movie",
+    };
+  }
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
 
-export default function Search(props) {
-  return (
-    <div>
-      <Form>
-        <Row>
-          <Col>
-            <Form.Control
-              onChange={(e) => props.onChange(e)}
-              placeholder="Movie Title"
-            />
-          </Col>
-          <Col>
-            <Form.Control placeholder="Movie Year" />
-          </Col>
-          <Col>
-            <Form.Check custom inline label="Movies" />
-            <Form.Check custom inline label="Series" />
-            <Form.Check custom inline label="Shows" />
-          </Col>
-        </Row>
-      </Form>
-      <Button onClick={() => props.searchMovies()}>Search</Button>
-    </div>
-  );
+  searchMovies = async () => {
+    const { input, year, type } = this.state;
+    if (year !== "" && input === "") return;
+
+    // if (input === "" && year === "") return;
+    console.log(input);
+    await movieStore.addToSearchList(input, year, type);
+  };
+
+  render() {
+    return (
+      <div>
+        <Form>
+          <Row>
+            <Col>
+              <Row className="text-center">
+                <Col sm={4}>
+                  <Form.Control
+                    onChange={this.onChange}
+                    placeholder="Title"
+                    name="input"
+                  />
+                </Col>
+                <Col sm={4}>
+                  <Form.Control
+                    onChange={this.onChange}
+                    name="year"
+                    placeholder="Year"
+                  />
+                </Col>
+                <Col sm={4}>
+                  <Form.Control
+                    name="type"
+                    onChange={this.onChange}
+                    as="select"
+                  >
+                    <option name="movie" value="movie" defaultValue>
+                      Movie
+                    </option>
+                    <option name="series" value="series">
+                      Series
+                    </option>
+                    <option name="episode" value="episode">
+                      Episode
+                    </option>
+                  </Form.Control>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Form>
+        <Container>
+          <Row>
+            <Col className="text-center">
+              <Button
+                size="lg"
+                className="mt-3 mb-4"
+                onClick={this.searchMovies}
+              >
+                Search
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
