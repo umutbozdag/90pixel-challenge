09c83f6efe4c79d0eb98ed679d13294841558c49
Umut <bozdag321@hotmail.com>
2020-04-21 16:50:43 +0300

    add google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles.scss";
+import "../styles/style.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Head from "next/head";
 
@@ -12,6 +12,10 @@ function MyApp({ Component, pageProps }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <Component {...pageProps} />
     </div>
