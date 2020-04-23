08b10d157071d4241b71eff0f2d750140d3408f4
Umut <bozdag321@hotmail.com>
2020-04-23 20:42:37 +0300

    add route for add page

--- pages/favorites.js
+++ pages/favorites.js
@@ -3,7 +3,8 @@ import { observer } from "mobx-react";
 import Movies from "../components/Movies/Movies";
 import { Container } from "react-bootstrap";
 import movieStore from "../store/movieStore";
-
+import Link from "next/link";
+import CommonTitle from "../components/CommonTitle/CommonTitle";
 @observer
 export default class Favorites extends React.Component {
   constructor(props) {
@@ -15,13 +16,24 @@ export default class Favorites extends React.Component {
   }
   render() {
     return (
-      <div>
+      <>
         <Nav />
         <Container>
-          <h3 className="mt-4 mb-4">Your Favorites</h3>
+          <CommonTitle>
+            <h1 className="text-center mt-5">Your Favorites</h1>
+          </CommonTitle>
+          <h6 className="mt-4 text-center">
+            <small> Couldn't you find the thing you are looking for? </small>
+            <Link href="/add">
+              <a>
+                <small>Add it yourself! </small>
+              </a>
+            </Link>
+          </h6>
+          <h3 className="mt-4 mb-4"></h3>
           <Movies movies={[...movieStore.favorites]} />
         </Container>
-      </div>
+      </>
     );
   }
 }
