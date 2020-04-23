14ba54b4e4d17f1a617ab0862c4a30c0767ee115
Umut <bozdag321@hotmail.com>
2020-04-23 20:43:13 +0300

    make cards responsive

--- components/Movies/Movies.js
+++ components/Movies/Movies.js
@@ -1,34 +1,42 @@
 import MovieCard from "../MovieCard/MovieCard";
-import { Container, Row, Col, Spinner, CardDeck } from "react-bootstrap";
+import { Container, Row, Col, Spinner, CardDeck, Toast } from "react-bootstrap";
 import movieStore from "../../store/movieStore";
+import { useState } from "react";
 
-const Movies = (props) => (
-  <Row>
-    <Container>
-      <Row>
-        <Col className="text-center">
-          {movieStore.loading ? (
-            <Spinner className="mt-5" animation="border" />
-          ) : (
-            ""
-          )}
-        </Col>
-      </Row>
-      <Row>
-        <Col className="text-center">
-          {movieStore.error ? <p>{movieStore.error}</p> : ""}
-        </Col>
-      </Row>
-    </Container>
+export default function Movies(props) {
+  return (
     <Row>
-      {props.movies &&
-        props.movies.map((movie) => (
-          <Col key={movie.imdbID} sm>
-            <MovieCard key={movie.imdbID} movie={movie} />
+      <Container>
+        <Row>
+          <Col className="text-center">
+            {movieStore.loading ? (
+              <Spinner className="mt-5" animation="border" />
+            ) : (
+              ""
+            )}
           </Col>
-        ))}
+        </Row>
+        <Row>
+          <Col className="text-center">
+            {movieStore.error ? <p>{movieStore.error}</p> : ""}
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row
+          className="d-flex justify-content-center"
+          style={{ marginLeft: 0, marginRight: 0 }}
+        >
+          {props.movies &&
+            props.movies.map((movie) => (
+              <Row>
+                <Col xs={8} sm={8} md={6} key={movie.imdbID}>
+                  <MovieCard movie={movie} />
+                </Col>
+              </Row>
+            ))}
+        </Row>
+      </Container>
     </Row>
-  </Row>
-);
-
-export default Movies;
+  );
+}
